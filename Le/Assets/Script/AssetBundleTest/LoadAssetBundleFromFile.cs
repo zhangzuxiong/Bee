294095102b4b2eeb59834c3b956272085359be15
zhangzuxiong <762637517@qq.com>
2023-12-05 18:29:36 +0800

    bundles

--- Le/Assets/Script/AssetBundleTest/LoadAssetBundleFromFile.cs
+++ Le/Assets/Script/AssetBundleTest/LoadAssetBundleFromFile.cs
@@ -1,12 +1,29 @@
 using System;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class LoadAssetBundleFromFile : MonoBehaviour
 {
+
+    public Image img;
     private void Start()
     {
         AssetBundle bundle = AssetBundle.LoadFromFile("AssetBundles/main/go");
-        GameObject go = bundle.LoadAsset<GameObject>("Capsule");
-        Instantiate(go);
+        GameObject[] gos = bundle.LoadAllAssets<GameObject>();
+        foreach (var go in gos)
+        {
+            Instantiate(go);
+        }
+
+        bundle = AssetBundle.LoadFromFile("AssetBundles/tex");
+        Sprite[] sprites = bundle.LoadAllAssets<Sprite>();
+        int index = 0;
+        foreach (var sp in sprites)
+        {
+            Image tmp = Instantiate(img, img.transform.parent);
+            tmp.rectTransform.anchoredPosition = img.rectTransform.anchoredPosition + new Vector2(150 * index, 0);
+            index++;
+            tmp.sprite = sp;
+        }
     }
 }
